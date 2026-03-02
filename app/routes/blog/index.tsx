34293cdcf101f79c4059cf1213f1fa044384c6d4
Tofu Gang <tofugangsw@gmail.com>
2026-03-02 16:09:16 +0100

    implement filtering blog posts by a query string

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -4,6 +4,7 @@ import type { PostMeta } from "~/types";
 import { useState } from "react";
 import Pagination from "~/components/Pagination";
 import PostCard from "~/components/PostCard";
+import PostFilter from "~/components/PostFilter";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -37,22 +38,40 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{posts: Pos
 // TODO: As with layouts, rename to BlogPage?
 function Blog({ loaderData }:Route.ComponentProps) {
     const { posts } = loaderData;
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const filteredPosts = posts.filter((post) => {
+        const query = searchQuery.toLowerCase();
+        return (
+            post.title.toLowerCase().includes(query) ||
+            post.excerpt.toLowerCase().includes(query)
+        );
+    });
+
     const [currentPage, setCurrentPage] = useState(1);
     // TODO: move to some sort of global settings?
     const postsPerPage = 2;
     // calculate total pages
-    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
     // get current page blog posts
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+    const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
 
     return (
         <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
             <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
-            {currentPosts.map((post) => (
-                <PostCard key={post.slug} post={post} />
-            ))}
+            <PostFilter searchQuery={searchQuery} onSearchChange={(query) => {
+                setSearchQuery(query);
+                setCurrentPage(1);
+            }} />
+            <div className="space-y-8">
+                {currentPosts.length === 0 ?
+                    <p className="text-gray-400 text-center">No posts found</p> :
+                    currentPosts.map((post) =>
+                        <PostCard key={post.slug} post={post} />
+                    )}
+            </div>
             <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={setCurrentPage} />
         </div>
     );
