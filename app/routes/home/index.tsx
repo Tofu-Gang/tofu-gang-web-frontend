01050d6e33862572a04c45c230be4bb36a38ec4c
Tofu Gang <tofugangsw@gmail.com>
2026-03-02 16:59:05 +0100

    show number of latest posts on the homepage

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,8 +1,9 @@
 import type { Route } from "./+types/index";
 import FeaturedProjects from "~/components/FeaturedProjects";
-import type { Project } from "~/types";
+import type { Project, PostMeta } from "~/types";
 import axios, { type AxiosResponse } from "axios";
 import AboutPreview from "~/components/AboutPreview";
+import LatestPosts from "~/components/LatestPosts";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -16,20 +17,34 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-export async function loader({ request }: Route.LoaderArgs):Promise<{projects: Project[]}> {
-    const response: AxiosResponse<Project[]> = await axios.get(`${import.meta.env.VITE_API_URL}/projects`);
-    return { projects: response.data.filter((project) => project.featured) };
+export async function loader({ request }: Route.LoaderArgs):Promise<{projects: Project[]; posts: PostMeta[]}> {
+    const projectsResponse: AxiosResponse<Project[]> = await axios.get(`${import.meta.env.VITE_API_URL}/projects`);
+    const url = new URL("/posts-meta.json", request.url);
+    const postsResponse: AxiosResponse<PostMeta[]> = await axios.get(url.href);
+
+    if(projectsResponse.status !== 200 || postsResponse.status !== 200) {
+        throw new Error("Failed to fetch projects or posts");
+    } else {
+        const projects = projectsResponse.data.filter((project) => project.featured);
+        const posts = postsResponse.data;
+        // sort desc by date (newest first)
+        posts.sort((a: PostMeta, b: PostMeta) => {
+            return new Date(b.date).getTime() - new Date(a.date).getTime();
+        });
+        return { projects, posts };
+    }
 }
 
 // TODO: As with layouts, rename to HomePage?
 function Home({ loaderData }: Route.ComponentProps) {
-    const { projects } = loaderData as {projects: Project[]};
+    const { projects, posts } = loaderData as {projects: Project[]; posts: PostMeta[]};
 
     return (
         <>
             {/* TODO: move count to some sort of global settings? */}
             <FeaturedProjects projects={projects} count={2} />
             <AboutPreview />
+            <LatestPosts posts={posts} limit={3} />
         </>
     );
 }
